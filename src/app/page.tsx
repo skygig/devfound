b60e411d345f76985998dc44e773105ecb8172c0
Akash Singh <developer.akash8@gmail.com>
2025-05-12 02:57:01 +0530

    use: sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h2>Devfound</h2>
+      <button>hello</button>
     </div>
   );
 }
