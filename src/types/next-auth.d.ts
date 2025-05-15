cf096792c8466a760263dcd34c52066a4dfb432e
Akash Singh <developer.akash8@gmail.com>
2025-05-16 00:34:19 +0530

    fix: next-auth session type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,12 @@
+import NextAuth from "next-auth";  // eslint-disable-line @typescript-eslint/no-unused-vars
 
 declare module "next-auth" {
     interface Session {
+        userId?: string;
         user: {
             name?: string | null;
             email?: string | null;
             image?: string | null;
-            userId?: string;
         };
     }
 }
