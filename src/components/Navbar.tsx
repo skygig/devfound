8ee5633f81b95f5fc7227a82862a216201c574b8
Akash Singh <developer.akash8@gmail.com>
2025-05-15 08:52:25 +0530

    navbar: handle navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,27 @@
 "use client";
 
 import Image from "next/image"
+import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { signIn, signOut, useSession } from "next-auth/react";
-
 
 import styles from "@/styles/navbar.module.scss"
 import logo from "@/assets/svgs/logo.svg"
 
 const Navbar = () => {
+    const router = useRouter()
     const { data: session } = useSession();
     const [userPop, setUserPop] = useState(false);
 
     return <nav className={styles.nav}>
-        <div className={styles.title}>
+        <div className={styles.title} onClick={() => router.push("/")}>
             <Image src={logo} alt="Devfound Logo" />
             <h1>Devfound</h1>
         </div>
 
-        <div>
+        <div className={styles.options}>
+            <Link href="/repos">all repos</Link>
             {
                 session ? <div className={styles.user}>
                     <img src={session.user?.image ?? ""} alt="avatar" onClick={() => setUserPop(!userPop)} />
