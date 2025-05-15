0f84118d33960b15c7adb568b9b1b3a13e2bac08
Akash Singh <developer.akash8@gmail.com>
2025-05-15 09:32:17 +0530

    footer: add links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,13 @@ import styles from "@/styles/footer.module.scss";
 
 const Footer = () => {
     return <footer className={styles.footer}>
-        © 2025 Devfound. All rights reserved.
+        <div className={styles.links}>
+            <a href="https://www.linkedin.com/in/akash-singh8" target="_blank">contact</a>
+            <a href="https://github.com/skygig/devfound" target="_blank">github</a>
+        </div>
+        <p>
+            © 2025 Devfound. All rights reserved.
+        </p>
     </footer>
 }
 
