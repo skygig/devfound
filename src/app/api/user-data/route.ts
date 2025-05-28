2a8ef44c7d289c9828c4273e38a01997e20603e6
Akash Singh <developer.akash8@gmail.com>
2025-05-29 00:51:06 +0530

    store user-data on db

--- src/app/api/user-data/route.ts
+++ src/app/api/user-data/route.ts
@@ -1,21 +1,56 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+
+import { prisma } from "@/libs/prisma";
+import { authOptions } from "../auth/[...nextauth]/route";
 import fetchUserData from "@/app/api/user-data/fetchUserData";
 
 export const GET = async (req: NextRequest) => {
-    const { searchParams } = new URL(req.url);
-    const userId = searchParams.get("userId")
+  const session = await getServerSession(authOptions);
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get("userId");
 
-    if (!userId) {
-        return NextResponse.json({ message: "Invalid request, required userId!" }, { status: 403 })
+  if (!userId || !session) {
+    return NextResponse.json(
+      {
+        message: "Invalid request, required userId and user must be logged in!",
+      },
+      { status: 403 }
+    );
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { userId },
+    });
+    if (user) {
+      return NextResponse.json({ ...user }, { status: 200 });
     }
 
-    // TODO: check if the user exists in the db and then only fetch the user details
-    try {
-        const { languages, starredRepos } = await fetchUserData(userId);
-        return NextResponse.json({ languages, starredRepos }, { status: 200 })
-    } catch (err) {
-        console.error("Unable to fetch user details", err)
-        return NextResponse.json({ message: "Server Side Error." }, { status: 500 })
-    }
-}
+    const { languages, starredRepos, frameworks, tools } = await fetchUserData(
+      userId
+    );
 
+    await prisma.user.create({
+      data: {
+        userId: session.userId,
+        name: session.user?.name,
+        languages,
+        starredRepos,
+        frameworks,
+        tools,
+      },
+    });
+
+    return NextResponse.json(
+      { languages, starredRepos, frameworks, tools },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("Unable to fetch user details", err);
+    return NextResponse.json(
+      { message: "Server Side Error." },
+      { status: 500 }
+    );
+  }
+};
